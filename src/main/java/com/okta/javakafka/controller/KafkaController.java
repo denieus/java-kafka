c7050db55f0bfcf75f96874a295c975e0fb09ea1
Daniel Pereira <danielpereira15@gmail.com>
2019-11-04 21:27:47 +0100

    Initial commit

---
+++ src/main/java/com/okta/javakafka/controller/KafkaController.java
@@ -0,0 +1,31 @@
+package com.okta.javakafka.controller;
+
+import com.okta.javakafka.consumer.MyTopicConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    @Autowired
+    private MyTopicConsumer myTopicConsumer;
+
+    @GetMapping("/kafka/produce")
+    public void produce(@RequestParam String message) {
+        template.send("my_topic", message);
+    }
+
+    @GetMapping("/kafka/messages")
+    public List<String> getMessages() {
+        return myTopicConsumer.getMessages();
+    }
+
+}
